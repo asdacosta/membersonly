8d75f02ad46022644af592b36e89b3149a1be533
asdacosta <asdacosta18@gmail.com>
2024-10-26 07:15:11 +0000

    Set up sessions and passport well

--- app.js
+++ app.js
@@ -2,20 +2,33 @@ const express = require("express");
 const path = require("path");
 const session = require("express-session");
 const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-const { postSignUp, signUpValidation } = require("./controllers/control");
+const {
+  postSignUp,
+  signUpValidation,
+  localStrategy,
+  deserialize,
+  postJoinClub,
+} = require("./controllers/control");
 const app = express();
 const assetsPath = path.join(__dirname, "public");
 
-app.use(express.static(assetsPath));
-app.use(passport.session());
-app.use(express.urlencoded({ extended: false }));
-app.use(session({ secret: "Vybz", resave: false, saveUninitialized: false }));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.static(assetsPath));
+app.use(express.urlencoded({ extended: false }));
+app.use(session({ secret: "Vybz", resave: false, saveUninitialized: false }));
+app.use(passport.session());
+
+passport.use(localStrategy);
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser(deserialize);
+
 app.get("/", (req, res) => res.render("index"));
 app.get("/sign-up", (req, res) => res.render("sign-up"));
+app.get("/join-club", (req, res) => res.render("join-club"));
+
+app.post("/join-club", postJoinClub);
 app.post("/sign-up", signUpValidation, postSignUp);
 
 const PORT = 3000;
