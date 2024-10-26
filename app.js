4d40b0bda1c46b98f4feaa7d03eca5229241fcf1
asdacosta <asdacosta18@gmail.com>
2024-10-26 11:41:43 +0000

    Set up logIn and logOut on app

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const {
   localStrategy,
   deserialize,
   postJoinClub,
+  postLogIn,
+  getLogOut,
 } = require("./controllers/control");
 const app = express();
 const assetsPath = path.join(__dirname, "public");
@@ -24,12 +26,21 @@ passport.use(localStrategy);
 passport.serializeUser((user, done) => done(null, user.id));
 passport.deserializeUser(deserialize);
 
-app.get("/", (req, res) => res.render("index"));
+app.get("/", (req, res) => res.render("index", { user: req.user }));
 app.get("/sign-up", (req, res) => res.render("sign-up"));
 app.get("/join-club", (req, res) => res.render("join-club"));
+app.get("/log-in", (req, res) => res.render("log-in"));
+app.get("/log-out", getLogOut);
 
 app.post("/join-club", postJoinClub);
 app.post("/sign-up", signUpValidation, postSignUp);
+app.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/members",
+    failureRedirect: "/log-in",
+  })
+);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Port ${PORT} ongoing!`));
