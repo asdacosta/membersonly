cce044327f0290de51651b2cb6b58d91c7c7e0a4
asdacosta <asdacosta18@gmail.com>
2024-10-26 07:19:44 +0000

    Define subsequent queries

--- db/queries.js
+++ db/queries.js
@@ -1,4 +1,4 @@
-const pool = require("./db/Pool");
+const pool = require("../db/Pool");
 
 async function addUser(firstName, lastName, email, password) {
   await pool.query(
@@ -7,16 +7,58 @@ async function addUser(firstName, lastName, email, password) {
   );
 }
 
-async function findUser(username) {
+async function findUser(user) {
   const { rows } = await pool.query(
     "SELECT username FROM users WHERE username = $1",
+    [user]
+  );
+  const username = rows[0];
+  return username;
+}
+
+async function findAllUserData(username) {
+  const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [
+    username,
+  ]);
+  const user = rows[0];
+  return user;
+}
+
+async function findUserWithId(id) {
+  const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+  const user = rows[0];
+  return user;
+}
+
+async function updateMembership(username) {
+  const { rows } = await pool.query(
+    "SELECT membership_status FROM users WHERE username = $1",
     [username]
   );
-  const { username } = rows[0];
-  return username;
+  const membership_status = rows[0];
+  if (membership_status === "active") return false;
+
+  await pool.query(
+    "UPDATE users SET membership_status = 'active' WHERE username = $1 ",
+    [username]
+  );
+  return true;
+}
+
+async function checkMembership(username) {
+  const { rows } = await pool.query(
+    "SELECT membership_status FROM users WHERE username = $1",
+    [username]
+  );
+  const user = rows[0];
+  return user;
 }
 
 module.exports = {
   addUser,
   findUser,
+  updateMembership,
+  findAllUserData,
+  findUserWithId,
+  checkMembership,
 };
